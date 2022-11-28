f467537bfa5bb542bbb6a39871570fd0705e8335
Nimbus15 <Alex.ZBP2456@GMAIL.COM>
2022-11-28 14:57:21 +0000

    Started and done task3, task2 - getCountDownText function()

--- problem2/scripts.js
+++ problem2/scripts.js
@@ -15,7 +15,11 @@ function createAnElement(){
     buttonElement.innerText = "Start Countdown";
 
     const containerElement = document.createElement("div");
-    containerElement.innerText = "0:0:0";
+    containerElement.innerText = getCountDownText({
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
 
     const countdownContainerElement = document.getElementById("countdown-container");
 
@@ -30,8 +34,39 @@ function createAnElement(){
     })).subscribe();
 }
 
+// Format an countdown object to a countdown string
+function getCountDownText(countdown){
+  const hours = `${countdown.hours < 10 ? "0" : ""}${countdown.hours}`;
+  const minutes = `${countdown.minutes < 10 ? "0" : ""}${
+    countdown.minutes
+  }`;
+  const seconds = `${countdown.seconds < 10 ? "0" : ""}${
+    countdown.seconds
+  }`;
+
+  return `${hours}:${minutes}:${seconds}`;
+}       
+
 //Turn totalSeconds into "hours, minutes, seconds" object
 function getCountDownValues(totalSeconds){
-  
-}
+    const result = { hours: 0, minutes: 0, seconds: 0 };
 
+    if (totalSeconds >= 1) {
+    let secondsForCalculation = totalSeconds;
+
+    result.hours = secondsForCalculation / 3600;
+    result.hours = result.hours >= 1 ? parseInt(result.hours) : 0;
+    secondsForCalculation = secondsForCalculation - result.hours * 3600;
+
+    //MINUTES
+
+    result.minutes = secondsForCalculation / 60;
+    result.minutes = result.minutes >= 1 ? parseInt(result.minutes) : 0;
+    secondsForCalculation = secondsForCalculation - result.minutes * 60;
+
+    //SECONDS
+
+    result.seconds = secondsForCalculation;
+  }
+  return result
+}
